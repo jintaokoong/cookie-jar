0fce399d4165a1755f378fb01c47a7d5c2306255
Jintao Koong <jintaokoong99@gmail.com>
2023-05-03 16:41:33 +0800

    docs: add comments documenting findings

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowCredentials: true,
-		AllowOrigins:     "http://localhost:5173",
+		AllowCredentials: true,                    // we need this to allow the cookie to be sent
+		AllowOrigins:     "http://localhost:5173", // whitelist the frontend domain
 	}))
 
 	// declare health check route
@@ -26,9 +26,13 @@ func main() {
 		})
 	})
 
+	// we set the cookie on signin
 	app.Post("/api/auth/signin", auth.Authenticate)
+
+	// we destroy the cookie on signout
 	app.Post("/api/auth/signout", auth.RemoveSession)
 
+	// we define a route that is required to have a valid refresh token
 	app.Get("/api/secure/resource", func(c *fiber.Ctx) error {
 		cookie := c.Cookies("refresh_token")
 		if cookie == "token_value" {
--- routers/auth/init.go
+++ routers/auth/init.go
@@ -8,7 +8,8 @@ func Authenticate(c *fiber.Ctx) error {
 	c.Cookie(&fiber.Cookie{
 		Name:     "refresh_token",
 		Value:    "token_value",
-		SameSite: "lax",
+		Domain:   "localhost", // setting domain to hoge.com will make the cookie accessible for both fuga.hoge.com and piyo.hoge.com
+		SameSite: "strict",    // the domain setting allows us to set the SameSite attribute to strict, which is the most secure option
 		HTTPOnly: true,
 	})
 	return c.JSON(fiber.Map{
